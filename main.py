faeadd459b0c028819b1db63096fb2b7444f2e66
Evgeniy Pelikh <eugene.p9g@gmail.com>
2024-10-17 12:46:54 +0200

    add lifespan async func

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
+from contextlib import asynccontextmanager
 from typing import Annotated
 
 import uvicorn
 from fastapi import Depends, FastAPI
 from pydantic import BaseModel
 
-app = FastAPI()
+from database import create_tables, delete_tables
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await delete_tables()
+    print('All data from DB have been deleted!')
+
+    await create_tables()
+    print('Created a new empty tabels!')
+
+    yield
+    print('Shutdown')
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 class TaskAdd(BaseModel):
