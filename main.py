0787fd63bcfa33f883a83444ac71eb909233f131
Evgeniy Pelikh <eugene.p9g@gmail.com>
2024-10-17 15:04:59 +0200

    replace all code by files, cut all routes and shemas

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from contextlib import asynccontextmanager
-from typing import Annotated
 
-import uvicorn
-from fastapi import Depends, FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI
 
 from database import create_tables, delete_tables
 
@@ -21,31 +18,3 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
-
-
-class TaskAdd(BaseModel):
-    name: str
-    description: str | None = None
-
-
-class Task(TaskAdd):
-    id: int
-
-
-tasks = []
-
-
-@app.post('/tasks')
-async def add_task(task: Annotated[TaskAdd, Depends()]):
-    return 'ok'
-
-
-@app.get('/tasks')
-def get_tasks():
-    task = Task(name='Create a new task!')
-
-    return {'data': task}
-
-
-if __name__ == '__main__':
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
