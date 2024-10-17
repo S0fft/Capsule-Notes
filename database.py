e026a109e0aafc6c1d96281bf68000d412c42be0
Evgeniy Pelikh <eugene.p9g@gmail.com>
2024-10-17 11:55:59 +0200

    add async new_session for db requests

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
-from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 
 engine = create_async_engine(
     'sqlite+aiosqlite:///tasks.db'
 )
+new_session = async_sessionmaker(engine, expire_on_commit=False)
