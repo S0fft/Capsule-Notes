f31845940c5c944d8387a6c201baa94e8cb20969
Evgeniy Pelikh <eugene.p9g@gmail.com>
2024-10-17 16:23:56 +0200

    add tags, add Ok response for post_task method

--- routes.py
+++ routes.py
@@ -3,22 +3,23 @@ from typing import Annotated
 from fastapi import APIRouter, Depends
 
 from repository import TaskRepository
-from schemas import TaskAdd
+from schemas import Task, TaskAdd, TaskId
 
 router = APIRouter(
     prefix='/tasks',
+    tags=['Tasks']
 )
 
 
 @router.post('')
-async def post_task(task: Annotated[TaskAdd, Depends()]):
+async def post_task(task: Annotated[TaskAdd, Depends()]) -> TaskId:
     task_id = await TaskRepository.add_data(task)
 
     return {'Ok!': True, 'task_id': task_id}
 
 
 @router.get('')
-async def get_tasks():
-    tasks = await TaskRepository.find_all(tasks)
+async def get_tasks() -> list[Task]:
+    tasks = await TaskRepository.get_data()
 
-    return {'data': tasks}
+    return tasks
