72ec1e2f35f4716d1ebd174cf123f587cf3d9fac
Evgeniy Pelikh <eugene.p9g@gmail.com>
2024-10-17 12:41:03 +0200

    fix mistake with func call - remove ()

--- database.py
+++ database.py
@@ -21,9 +21,9 @@ class TaskTable(BaseModel):
 
 async def create_tables():
     async with engine.begin() as conn:
-        await conn.run_sync(BaseModel.metadata.create_all())
+        await conn.run_sync(BaseModel.metadata.create_all)
 
 
 async def delete_tables():
     async with engine.begin() as conn:
-        await conn.run_sync(BaseModel.metadata.drop_all())
+        await conn.run_sync(BaseModel.metadata.drop_all)
