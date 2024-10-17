79efa1f321abf185f154522bf659184ca9ffa601
Evgeniy Pelikh <eugene.p9g@gmail.com>
2024-10-17 12:18:32 +0200

    add delete_tables async func for db (for ease of development)

--- database.py
+++ database.py
@@ -22,3 +22,8 @@ class TaskTable(BaseModel):
 async def create_tables():
     async with engine.begin() as conn:
         await conn.run_sync(BaseModel.metadata.create_all())
+
+
+async def delete_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(BaseModel.metadata.drop_all())
