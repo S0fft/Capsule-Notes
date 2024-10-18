b913a9a15312a4259cec6cda3e1d0b0b6d40aa9e
Evgeniy Pelikh <eugene.p9g@gmail.com>
2024-10-18 13:53:56 +0200

    add new route for patch request

--- routes.py
+++ routes.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, HTTPException
+from sqlalchemy.exc import NoResultFound
 
 from repository import TaskRepository
 from schemas import Task, TaskAdd
@@ -38,6 +39,15 @@ async def update_task(task_id: int, task_data: TaskAdd) -> Task:
         raise HTTPException(status_code=404, detail="Task not found!")
 
     return updated_task
+
+
+@router.patch('/{task_id}', response_model=Task)
+async def patch_task(task_id: int, task_data: TaskAdd) -> Task:
+    try:
+        updated_task = await TaskRepository.patch_data(task_id, task_data.dict(exclude_unset=True))
+        return updated_task
+    except NoResultFound:
+        raise HTTPException(status_code=404, detail=f"Task with id {task_id} not found!")
 
 
 @router.delete('/{task_id}')
