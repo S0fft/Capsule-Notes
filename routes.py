48017c43dbcbfbb2bd6e3d896313c70dc5af75b3
Evgeniy Pelikh <eugene.p9g@gmail.com>
2024-10-18 13:08:18 +0200

    pep8 corrections, add spacing

--- routes.py
+++ routes.py
@@ -26,14 +26,18 @@ async def get_tasks() -> list[Task]:
 @router.put('/{task_id}', response_model=Task)
 async def update_task(task_id: int, task_data: TaskAdd) -> Task:
     updated_task = await TaskRepository.update_data(task_id, task_data)
+
     if not updated_task:
         raise HTTPException(status_code=404, detail="Task not found")
+
     return updated_task
 
 
 @router.delete('/{task_id}')
 async def delete_task(task_id: int):
     deleted = await TaskRepository.delete_data(task_id)
+
     if not deleted:
         raise HTTPException(status_code=404, detail="Task not found")
+
     return {"message": "Task deleted"}
