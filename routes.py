e26729dba15b9bbd3956e550f41c729231908a79
Evgeniy Pelikh <eugene.p9g@gmail.com>
2024-10-18 12:53:43 +0200

    update get and post methods, create put and delete methods for future CRUD

--- routes.py
+++ routes.py
@@ -1,6 +1,6 @@
-from typing import Annotated
+from typing import Annotated, List
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 
 from repository import TaskRepository
 from schemas import Task, TaskAdd, TaskId
@@ -11,11 +11,11 @@ router = APIRouter(
 )
 
 
-@router.post('')
-async def post_task(task: Annotated[TaskAdd, Depends()]) -> TaskId:
-    task_id = await TaskRepository.add_data(task)
+@router.post('', response_model=Task)
+async def post_task(task: TaskAdd) -> Task:
+    new_task = await TaskRepository.add_data(task)
 
-    return {'Ok!': True, 'task_id': task_id}
+    return new_task
 
 
 @router.get('')
@@ -23,3 +23,19 @@ async def get_tasks() -> list[Task]:
     tasks = await TaskRepository.get_data()
 
     return tasks
+
+
+@router.put('/{task_id}', response_model=Task)
+async def update_task(task_id: int, task_data: TaskAdd) -> Task:
+    updated_task = await TaskRepository.update_data(task_id, task_data)
+    if not updated_task:
+        raise HTTPException(status_code=404, detail="Task not found")
+    return updated_task
+
+
+@router.delete('/{task_id}')
+async def delete_task(task_id: int):
+    deleted = await TaskRepository.delete_data(task_id)
+    if not deleted:
+        raise HTTPException(status_code=404, detail="Task not found")
+    return {"message": "Task deleted"}
