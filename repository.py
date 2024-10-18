914a20150e010aa441a6cd2de822154571397f43
Evgeniy Pelikh <eugene.p9g@gmail.com>
2024-10-18 13:03:33 +0200

    add logic for delete and put methods, update get and post methods

--- repository.py
+++ repository.py
@@ -6,7 +6,7 @@ from schemas import Task, TaskAdd
 
 class TaskRepository:
     @classmethod
-    async def add_data(cls, data: TaskAdd) -> int:
+    async def add_data(cls, data: TaskAdd) -> Task:
         async with new_session() as session:
             task_dict = data.model_dump()
 
@@ -16,7 +16,7 @@ class TaskRepository:
             await session.flush()
             await session.commit()
 
-            return task.id
+            return Task.model_validate(task.__dict__)
 
     @classmethod
     async def get_data(cls) -> list[Task]:
@@ -28,3 +28,33 @@ class TaskRepository:
             tasks_schemas = [Task.model_validate(task_model.__dict__) for task_model in task_models]
 
             return tasks_schemas
+
+    @classmethod
+    async def update_data(cls, task_id: int, data: TaskAdd) -> Task | None:
+        async with new_session() as session:
+            query = select(TaskTable).where(TaskTable.id == task_id)
+            result = await session.execute(query)
+            task_model = result.scalars().first()
+
+            if not task_model:
+                return None
+
+            for key, value in data.model_dump().items():
+                setattr(task_model, key, value)
+
+            await session.commit()
+            return Task.model_validate(task_model.__dict__)
+
+    @classmethod
+    async def delete_data(cls, task_id: int) -> bool:
+        async with new_session() as session:
+            query = select(TaskTable).where(TaskTable.id == task_id)
+            result = await session.execute(query)
+            task_model = result.scalars().first()
+
+            if not task_model:
+                return False
+
+            await session.delete(task_model)
+            await session.commit()
+            return True
